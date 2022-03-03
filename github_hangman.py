c74386ebef3a710e66d6b801d08b9500ddf11d32
Yusuf Çalışır <99292717+stratocasting@users.noreply.github.com>
2022-03-03 17:36:25 +0300

    Add files via upload

---
+++ github_hangman.py
@@ -0,0 +1,108 @@
+word_list = ["ardvark", "baboon", "camel"]
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+import random
+
+
+
+end_of_game = False
+
+chosen_word = random.choice(word_list)
+word_length = len(chosen_word)
+
+lives = 6
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in range(word_length):
+    display += "_"
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    if guess in display:
+        print ("you have already gue")
+
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        # print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
+        if letter == guess:
+            display[position] = letter
+
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0:
+            print("you lose ! ")
+            end_of_game = True
+
+
+
+    print(f"{' '.join(display)}")
+
+
+    print (lives)
+
+
+
+    if "_" not in display:
+        end_of_game = True
+        print("You win.")
+
+    print (stages[lives])
